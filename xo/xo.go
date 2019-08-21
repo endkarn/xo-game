d3175526cfc348187feb72847fc1ce1fa237b678
endkarn <end_karn@hotmail.com>
2019-08-21 18:32:54 +0700

    [Edited]: แก้ไข Struct ของแต่ละ Player ให้แต่ละคนมีฟิลด์ Struct Player

--- xo/xo.go
+++ xo/xo.go
@@ -3,21 +3,37 @@ package xo
 type XoGame struct {
 	CurrentTurn int
 	PlayerTurn int
-	PlayerOneScore int
-	PlayerTwoScore int
-	GameBoard [3][3]string
+	PlayerOne Player
+	PlayerTwo Player
+	GameBoard [9]string
+}
+
+type Player struct {
+	Score int
+	Symbol string
 }
 
 func NewXoGame() XoGame {
 	return XoGame{
 		CurrentTurn:    0,
 		PlayerTurn:     0,
-		PlayerOneScore: 0,
-		PlayerTwoScore: 0,
-		GameBoard:      [3][3]string{},
+		PlayerOne: Player{
+			Score:  0,
+			Symbol: "O",
+		},
+		PlayerTwo: Player{
+			Score:  0,
+			Symbol: "X",
+		},
+		GameBoard:      [9]string{},
 	}
 }
 
-func (xoGame XoGame) HaveWinner() (bool , string) {
+func (xoGame *XoGame) HaveWinner() (bool , string) {
 	return false , "No Winner"
 }
+
+func (xoGame *XoGame) PlayerOnePlay(location int) {
+	xoGame.GameBoard[location] = xoGame.PlayerOne.Symbol
+}
+
